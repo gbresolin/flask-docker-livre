844f68a063899d7fc450dee48b4aa19eeb2ab38e
gbresolin <gregory.bresolin@ipilyon.net>
2020-03-10 15:52:09 +0100

    Update

--- book/__init__.py
+++ book/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def home():
+    return "Homepage!"
+
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
 
 
-if __name__ == '__main__':
-    app.run()
