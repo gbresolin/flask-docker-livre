e09f015ec5619d145f32935ce2fed3ae3ea66236
gbresolin <gregory.bresolin@ipilyon.net>
2020-03-22 18:22:08 +0100

    update

---
+++ book/error.py
@@ -0,0 +1,16 @@
+from flask import (
+    Blueprint, render_template
+)
+
+bp = Blueprint('error', __name__, url_prefix='/auth')
+
+
+# Errors
+@bp.errorhandler(404)
+def not_found(e):
+    return render_template("error/404.html"), 404
+
+
+@bp.errorhandler(403)
+def forbidden(e):
+    return render_template("error/403.html"), 403
